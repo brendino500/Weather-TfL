3ed3b5ec176058846c45b8ce39b238eea400ba31
brendino500 <brenda.ty@live.com>
2020-08-06 20:38:49 +0100

    background working. adding more styling

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -21,27 +21,6 @@ class Navbar extends React.Component {
 
   render() {
     return (
-    // <nav className="navbar is-transparent">
-    //   <div className="container">
-    //     <div className="navbar-brand">
-    //       <Link to="/" className="navbar-item">Home</Link>
-    //       <span onClick={this.handleNavToggle} className={`navbar-burger ${this.state.isOpen ? 'is-active' : ''}`}>
-    //         <span></span>
-    //         <span></span>
-    //         <span></span>
-    //       </span>
-    //     </div>
-    //     <div className={`navbar-menu ${this.state.isOpen ? 'is-active' : ''}`}>
-    //       <div className="navbar-end">
-    //         <Link to="/AirQuality" className="navbar-item">AirQuality</Link>
-    //         <Link to="/BikeStatus" className="navbar-item">BikeStatus</Link>
-    //         <Link to="/TubeStatus" className="navbar-item">TubeStatus</Link>
-    //         <Link to="/WeatherStatus" className="navbar-item">WeatherStatus</Link>
-    //       </div>
-    //     </div>  
-    //   </div>
-    // </nav>
-
       <header className="navbar is-transparent">
         <div className="container">
           <div className="navbar-brand">
@@ -54,7 +33,7 @@ class Navbar extends React.Component {
           </div>
           <div id="navbarMenuHeroC" className={`navbar-menu ${this.state.isOpen ? 'is-active' : ''}`}>
             <div className="navbar-end">
-              <Link to="/AirQuality"  className="navbar-item is-active">AirQuality</Link>
+              <Link to="/AirQuality"  className="navbar-item">AirQuality</Link>
               <Link to="/BikeStatus" className="navbar-item">BikeStatus</Link>
               <Link to="/TubeStatus" className="navbar-item">TubeStatus</Link>
               <Link to="/WeatherStatus" className="navbar-item">WeatherStatus</Link>
